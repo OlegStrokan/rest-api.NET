8f7847983b2f7e792fb3c56b664bae2167734e46
olehstrokan <oleh@wattstor.com>
2022-04-09 20:10:48 +0200

    v1.2.0. Added automapper for transform objects

---
+++ Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,14 @@
+
+using AutoMapper;
+
+namespace Notes.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile)
+        {
+            profile.CreateMap(typeof(T), GetType());
+        }
+    }
+}
+
