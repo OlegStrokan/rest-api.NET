ad9d003b5c11cd10fccb6053b9af08d7f9a1c91c
olehstrokan <oleh@wattstor.com>
2022-04-09 20:54:28 +0200

    v1.2.1. Added Create Note handler, create note Dto

--- Notes.Domain/Note.cs
+++ Notes.Domain/Note.cs
@@ -10,6 +10,6 @@ namespace Notes.Domain
         public string Title { get; set; }
         public string Details { get; set; }
         public DateTime CreationDate { get; set; }
-        public DateTime EditDate { get; set; }
+        public DateTime? EditDate { get; set; }
     }
 }
