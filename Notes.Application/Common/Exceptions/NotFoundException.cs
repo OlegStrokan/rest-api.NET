d1dd654c0b707a118520b2d899ee064c8befbf09
olehstrokan <oleh@wattstor.com>
2022-04-09 22:22:44 +0200

    v.1.3.0 Added Update Note handler, update note dto, own not found exception=

---
+++ Notes.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+namespace Notes.Application.Common.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) not found.")
+    {
+    }
+}
